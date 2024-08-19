f8d23a0b1927e2c1ee5aea80eb36f487c41e5820
ricar <ricardosr4@gmail.com>
2024-08-18 23:54:28 -0400

    creacion xml login ok

--- app/src/main/java/com/example/searchfriendsapp/ui/activity/login/presenter/LoginActivity.kt
+++ app/src/main/java/com/example/searchfriendsapp/ui/activity/login/presenter/LoginActivity.kt
@@ -1,21 +1,26 @@
 package com.example.searchfriendsapp.ui.activity.login.presenter
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.searchfriendsapp.R
+import com.example.searchfriendsapp.databinding.ActivityLoginBinding
+import com.example.searchfriendsapp.ui.activity.preLogin.presenter.PreLoginActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigation()
+    }
+
+    private fun navigation() {
+        binding.ivBack.setOnClickListener {
+            val intent = Intent(this, PreLoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
