00011b5f0e66c6f0aa37afb483a12fef5f54f147
ricar <ricardosr4@gmail.com>
2024-08-18 22:56:54 -0400

    creacion encarpetado

--- app/src/main/java/com/example/searchfriendsapp/ui/activity/preLogin/presenter/PreLoginActivity.kt
+++ app/src/main/java/com/example/searchfriendsapp/ui/activity/preLogin/presenter/PreLoginActivity.kt
@@ -1,21 +1,15 @@
 package com.example.searchfriendsapp.ui.activity.preLogin.presenter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.searchfriendsapp.R
+import com.example.searchfriendsapp.databinding.ActivityPreLoginBinding
 
 class PreLoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPreLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_pre_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityPreLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
