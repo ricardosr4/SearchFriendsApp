5d6be6e28801283a32a08a46e42c1d917829a2d5
ricar <ricardosr4@gmail.com>
2024-08-19 23:20:20 -0400

    navegacion ok

--- app/src/main/java/com/example/searchfriendsapp/ui/activity/homeContainer/HomeContainerActivity.kt
+++ app/src/main/java/com/example/searchfriendsapp/ui/activity/homeContainer/HomeContainerActivity.kt
@@ -1,15 +1,70 @@
 package com.example.searchfriendsapp.ui.activity.homeContainer
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.navigation.findNavController
+import com.example.searchfriendsapp.R
 import com.example.searchfriendsapp.databinding.ActivityHomeContainerBinding
+import com.google.android.material.navigation.NavigationView
 
-class HomeContainerActivity : AppCompatActivity() {
+class HomeContainerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityHomeContainerBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeContainerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toolbar: Toolbar = binding.root.findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> {
+                findNavController(R.id.nav_host_fragment_container).navigate(R.id.homeFragment)
+            }
+            R.id.nav_Terminos -> {
+                findNavController(R.id.nav_host_fragment_container).navigate(R.id.terminosFragment)
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/searchfriendsapp/ui/fragment/home/presenter/HomeFragment.kt
+++ app/src/main/java/com/example/searchfriendsapp/ui/fragment/home/presenter/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.searchfriendsapp.R
 import com.example.searchfriendsapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -25,4 +27,15 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvSearch.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
+        }
+        binding.tvRandom.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_randomFragment)
+        }
+
+    }
 }
