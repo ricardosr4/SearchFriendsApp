4126f5a66bfbfcd5e1de50392d5e8620d7fed89b
ricar <ricardosr4@gmail.com>
2024-08-19 22:40:17 -0400

    creacion xml home ok

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,28 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    //Picasso
+    implementation ("com.squareup.picasso:picasso:2.71828")
+    //Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.0")
+    //Navigation Component
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+    //Splash Scream
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+    //View Model
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0")
+    //LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.0")
+    //LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.0")
+
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
