ab15f9c73ab6c01c9f14c2e947d191eea4d99f9a
ricar <ricardosr4@gmail.com>
2024-08-30 23:46:58 -0400

    put extra ok...

--- app/src/main/java/com/example/searchfriendsapp/ui/fragment/home/adapter/AdapterHome.kt
+++ app/src/main/java/com/example/searchfriendsapp/ui/fragment/home/adapter/AdapterHome.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.searchfriendsapp.R
 import com.example.searchfriendsapp.databinding.ItemRvHomeBinding
 import com.squareup.picasso.Picasso
